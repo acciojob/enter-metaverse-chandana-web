a1f3b6081f121c808c243c2c8c5de3d7ebdc0746
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-23 23:02:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function update(){
+    const btn=document.getElementById("enterBtn")
+    const c=document.getElementById("status").textContent;
+    const newc =document.createElement('h1');
+    newc.textContent=c;
+    const parent =document.getElementById("status").parentNode;
+    parent.replaceChild(newc,document.getElementById('status'));
+}
+const btn = document.getElementById("enterBtn");
+btn.addEventListener("click",update);
